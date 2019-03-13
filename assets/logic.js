50fb7e20cf2c3ff5a3d562bbd9b3509fb7201fb9
steven <steven@stevens-MacBook-Pro.local>
2019-03-13 08:36:06 -0600

    The ajax link is not working

--- assets/logic.js
+++ assets/logic.js
@@ -1,5 +1,7 @@
 $("#find-giphy").on("click", function() {
-    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=To5ijLa1899Ime63nx4SwuXKYLoIb3Zm&q=&limit=25&offset=0&rating=G&lang=en"
+    let giphy=$(this).attr("data-gif");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?" +
+        giphy+"api_key=To5ijLa1899Ime63nx4SwuXKYLoIb3Zm";
     
       $.ajax({
         url: queryURL,
