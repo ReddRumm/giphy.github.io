2c1e3feea6da5800633ec80e30e876decb3d1793
steven <steven@stevens-MacBook-Pro.local>
2019-03-15 08:01:42 -0600

    s

--- assets/logic.js
+++ assets/logic.js
@@ -1,7 +1,7 @@
-$("#find-giphy").on("click", function() {
+$("button").on("click", function() {
     let giphy=$(this).attr("data-gif");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?" +
-        giphy+"api_key=To5ijLa1899Ime63nx4SwuXKYLoIb3Zm";
+    let queryURL = "https://api.giphy.com/v1/gifs/search?" +
+        giphy+"api_key=To5ijLa1899Ime63nx4SwuXKYLoIb3Zmm&q=&limit=25&offset=0&rating=G&lang=en";
     
       $.ajax({
         url: queryURL,
