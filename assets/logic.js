a3175dbd56759734c5aab0dcdf66e76e912e4305
steven <steven@stevens-MacBook-Pro.local>
2019-03-16 14:41:59 -0600

    sweet

--- assets/logic.js
+++ assets/logic.js
@@ -1,13 +1,13 @@
-$("button").on("click", function() {
-    let giphy=$(this).attr("data-gif");
-    let queryURL = "https://api.giphy.com/v1/gifs/search?" +
-        giphy+"api_key=To5ijLa1899Ime63nx4SwuXKYLoIb3Zmm&q=&limit=25&offset=0&rating=G&lang=en";
-    
-      $.ajax({
-        url: queryURL,
-        method: "GET"
-      }).then(function(response) {
-          console.log(response);
-      });
-    
-    });
+$("button").on("click", function () {
+  var giphy = $(this).attr("data-gif");
+  var queryURL = "https://api.giphy.com/v1/gifs/search?" +
+    giphy + "api_key=To5ijLa1899Ime63nx4SwuXKYLoIb3Zmm&q=&limit=25&offset=0&rating=G&lang=en";
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
+    console.log(response);
+  });
+
+});
