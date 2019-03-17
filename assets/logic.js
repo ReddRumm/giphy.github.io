4aaed7b2c65b0ed39c249405d75b1448b2029f1c
steven <steven@stevens-MacBook-Pro.local>
2019-03-17 08:50:48 -0600

    c

--- assets/logic.js
+++ assets/logic.js
@@ -1,16 +1,16 @@
-$("button").on("click", function () {
-  var giphy = $(this).attr("data-gif");
-  var queryURL = "https://api.giphy.com/v1/gifs/search?" +
-    giphy + "api_key=To5ijLa1899Ime63nx4SwuXKYLoIb3Zmm&q=&limit=25&offset=0&rating=G&lang=en";
+// $("button").on("click", function () {
+//   var giphy = $(this).attr("data-gif");
+//   var queryURL = "https://api.giphy.com/v1/gifs/search?" +
+//     giphy + "api_key=To5ijLa1899Ime63nx4SwuXKYLoIb3Zmm&q=&limit=25&offset=0&rating=G&lang=en";
 
   $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function (response) {
     console.log(response);
+    var gifImage = response.gif;
+    $('div').img(gifImage);
   });
 
-  var getGif= response.val().trim();
-  console.log(getGif);
 
-});
+// });
