7d0afe2aeb1ccd36662c6b3686fb9f186817ce2b
steven <steven@stevens-MacBook-Pro.local>
2019-03-19 10:15:47 -0600

    ok getting a lot closer

--- assets/logic.js
+++ assets/logic.js
@@ -5,15 +5,37 @@ Logic for index giphy
 steven midgley
 */
 
-var queryURL = 'https://api.giphy.com/v1/gifs/search?api_key=QgOLyan73JAYNbbtRUSTappfYi1DEp2&q=&limit=25&offset=0&rating=G&lang=en';
+var queryURL = 'https://api.giphy.com/v1/gifs/search?api_key=HQgOLyan73JAYNbbtRUSTappfYi1DEp2&q=fixie&limit=25&offset=0&rating=G&lang=en';
 
 $.ajax({
   url: queryURL,
   method: "GET"
 }).then(function (response) {
   console.log(response);
+  // After the data from the AJAX request comes back
+  // Saving the image_original_url property
+  gifImage=response.data.image_orifginal_url;
+  // Creating and storing an image tag
+  gifToHtml=$(`<img>`);
+  // Setting the gifToHtml src attribute to imageUrl
+  
 });
 
+
+
+
+
+
+// Prepending the catImage to the images div
+
+
+
+
+
+
+
+
+// =========================
 // save the responce in a var
 
 // make an array and store in a var
