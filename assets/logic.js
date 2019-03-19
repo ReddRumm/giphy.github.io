73c1fa47c1e59e37506ec59086135799f8d3740e
steven <steven@stevens-MacBook-Pro.local>
2019-03-19 09:42:44 -0600

    ok got some of it to work

--- assets/logic.js
+++ assets/logic.js
@@ -1,16 +1,17 @@
+
+console.log('cunt');
 /*
 Logic for index giphy
 steven midgley
 */
-var queryURL = 'https://api.giphy.com/v1/gifs/search?api_key=HQgOLyan73JAYNbbtRUSTappfYi1DEp2&q=&limit=25&offset=0&rating=G&lang=en';
+
+var queryURL = 'https://api.giphy.com/v1/gifs/search?api_key=QgOLyan73JAYNbbtRUSTappfYi1DEp2&q=&limit=25&offset=0&rating=G&lang=en';
 
 $.ajax({
   url: queryURL,
   method: "GET"
 }).then(function (response) {
   console.log(response);
-  var gifImage = response.gif;
-  $('div').img(gifImage);
 });
 
 // save the responce in a var
@@ -21,7 +22,6 @@ $.ajax({
 
 // loop through arrray
   // Add button attr to the array items
-
 // ========================================
 
 // make an on click function thats 
